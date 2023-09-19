a3df5a397a41dd4da75691df5c7e456a90cbdcb6
omerfaruksen <sen.faruk.93@gmail.com>
2023-09-20 00:34:09 +0300

    Desene Göre Metot Oluşturma

--- DeseneGoreMetotOlusturma/src/Main.java
+++ DeseneGoreMetotOlusturma/src/Main.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 public class Main {
 
-    static int rotate (int sayi ,int tempNumer){
-    if (sayi>=0){
-        System.out.print(sayi+" ");
-        rotate (sayi-5,tempNumer);
-    }
-        System.out.print(sayi+" ");
-    while (sayi>tempNumer){
-        sayi+=5;
-        System.out.print(sayi+" ");
-    }
+    static int rotate (int sayi ,int tempNumber){
+        //Recursive metot kullandığımızdan dolayı metot kendini her tekrarladığında sayı değişkenimiz değişiyor.
+        //Bu sebeple metot içerisinde tempNumber tanımlamak doğru olmayacağı için,
+        //metot parametresi olarak tempNumber adında bir değişken daha tanımladık.
+        //Bu sayede metot kendini her tekrarladığında sayı değişkeni yeni bir değer alırken,
+        // tempNumber daima kullanıcıdan aldığımız veri olarak saklanacak.
+        if (sayi>=0){
+            System.out.print(sayi+" ");
+            rotate (sayi-5,tempNumber); //program bu satıra geldiğinde otomatik olarak metodu baştan taramaya başlar.
+        }
+            System.out.print(sayi+" ");
+        while (sayi>tempNumber){
+            sayi+=5;
+            System.out.print(sayi+" ");
+        }
         return 1;
     }
 
-
-
     public static void main(String[] args) {
+        //Değişken tanımlamalarını yaptık.
         int i;
+        //kullanıcıdan verilerimizi aldık
         Scanner info=new Scanner(System.in);
         System.out.print("Desen Uzunluğunu Giriniz: ");
         i= info.nextInt();
+        //İki adet parametre aldığımız için kullanıcıdan aldığımız veriyi i,i şeklinde rotate metoduna gönderdik.
         rotate(i,i);
     }
 }
